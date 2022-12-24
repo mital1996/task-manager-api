55274634e3a18e68a22dbef946ce33734ac8f538
mital dholakiya <117336768+mital1996@users.noreply.github.com>
2022-12-24 11:24:17 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,15 +1,6 @@
 const sgMail = require("@sendgrid/mail");
-const sendgridAPIKey =
-  "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";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// sgMail.send({
-//   to: "mitaldholakiya1996@gmail.com",
-//   from: "mital1996dholakiya@gmail.com",
-//   subject: "This is my first creation!!",
-//   text: "I hope this one actually get to you.",
-// });
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
