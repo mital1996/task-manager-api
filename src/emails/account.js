50c1ac22f8542c55c772abefd5af1bc4c79c5c82
mital dholakiya <117336768+mital1996@users.noreply.github.com>
2022-12-26 16:21:39 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: "mital1996dholakiya@gmail.com",
+    from: process.env.FROM_EMAIL,
     subject: "Thanks for joining in!",
     text: `Welcome to the app, ${name}. Let me know how you get along with the app`,
   });
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationMail = (email, name) => {
   sgMail.send({
     to: email,
-    from: "mital1996dholakiya@gmail.com",
+    from: process.env.FROM_EMAIL,
     subject: "Sorry to see you go!",
     text: `GoodBye,${name}. I hope to see you sometime soon.`,
   });
